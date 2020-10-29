1dfa39e8bc0cbbab35d55766ba47d72927c192df
Mohamed Jubair <amjubair@outlook.com>
2020-10-29 05:32:33 +0530

    max-min using two pointer pattern

--- geeksforgeeks/max-min.js
+++ geeksforgeeks/max-min.js
@@ -1,19 +1,39 @@
-const solution = (arr) => {
-  const size = arr.length;
+// function solution(arr) {
+//   const size = arr.length;
+//   let isMaxTurn = true;
+//   const result = [];
+
+//   for (let i = 0; i < size; i++) {
+//     if (isMaxTurn) {
+//       result.push(arr.pop());
+//       isMaxTurn = false;
+//     } else {
+//       result.push(arr.shift());
+//       isMaxTurn = true;
+//     }
+//   }
+
+//   return result;
+// }
+
+function solution(arr) {
+  let leftPointer = 0;
+  let rightPointer = arr.length - 1;
   let isMaxTurn = true;
   const result = [];
-
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (isMaxTurn) {
-      result.push(arr.pop());
+      result.push(arr[rightPointer]);
+      rightPointer--;
       isMaxTurn = false;
     } else {
-      result.push(arr.shift());
+      result.push(arr[leftPointer]);
+      leftPointer++;
       isMaxTurn = true;
     }
   }
 
   return result;
-};
+}
 
 console.log(solution([1, 3, 4, 5, 8, 10]));
